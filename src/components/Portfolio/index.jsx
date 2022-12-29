51dd0510cbfdf245913bf69ad6edc0a7cc30d237
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2022-12-28 23:08:31 -0300

    feat: init conditional render

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 import { Element } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaintBrush } from "@fortawesome/free-solid-svg-icons";
 
 import SectionContainer from "components/SectionContainer";
 import { PortfolioSelection } from "./portfolio.styles";
+import projects from "../../database/projects.json";
 
 const Portfolio = () => {
+  const [showSelection, setShowSelectio] = useState(false);
+
+  const handleSelection = () => setShowSelectio(!showSelection);
+
   return (
     <SectionContainer
       icon={<FontAwesomeIcon icon={faPaintBrush} />}
@@ -13,7 +20,22 @@ const Portfolio = () => {
     >
       <Element name="portfolio" />
 
-      <PortfolioSelection>asadsad</PortfolioSelection>
+      <PortfolioSelection>
+        {!showSelection ? (
+          <div>
+            <button onClick={handleSelection}>select</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={handleSelection}> {"<<"} back </button>
+            <div>
+              {projects.map((project) => {
+                return <span>{project.title}</span>;
+              })}
+            </div>
+          </div>
+        )}
+      </PortfolioSelection>
     </SectionContainer>
   );
 };
